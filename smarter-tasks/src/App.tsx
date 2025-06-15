361b246cd11cdc88596d3a48131ac43defb189a5
Praneeth <praneeth@PRABHAASINI>
2025-06-15 06:18:08 +0000

    l3

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,11 @@
+import Task from "./Task";
+
+function App() {
+	return (
+		<div className="App text-3xl font-bold bg-blue-500">
+			<Task />
+		</div>
+	);
+}
+
+export default App;
---
+++ smarter-tasks/src/Task.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Task extends React.Component {
+	render() {
+		return <div>Buy groceries </div>;
+	}
+}
+export default Task;
