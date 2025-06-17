d8be9c777c4764f81518e22c21e9523b2aeeb707
Praneeth <praneeth@PRABHAASINI>
2025-06-17 03:57:08 +0000

    Doing l8

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
 
 import AccountLayout from "../layouts/account";
 import ProtectedRoute from "./ProtectedRoutes";
@@ -9,6 +9,9 @@ import MembersPage from "../pages/members";
 import Logout from "../pages/logout";
 import NotFound from "../pages/Notfound"; // make sure this component exists
 import { MembersProvider } from "../context/members/context";
+import ProjectContainer from "../pages/projects/ProjectContainer";
+import ProjectDetails from "../pages/project_details";
+import NewTask from "../pages/tasks/NewTask";
 const router = createBrowserRouter([
 	{ path: "/", element: <Navigate to="/account/projects" replace /> },
 	{ path: "/signin", element: <Signin /> },
@@ -25,21 +28,37 @@ const router = createBrowserRouter([
 		),
 		children: [
 			{ index: true, element: <Navigate to="/account/projects" replace /> },
-			{ path: "projects", element: <Projects /> },
-			{ path: "members", element: <MembersPage /> },
-		],
-	},
-	// Protected Routes
-	{
-		path: "account",
-		element: (
-			<ProtectedRoute>
-				<AccountLayout />
-			</ProtectedRoute>
-		),
-		children: [
-			{ index: true, element: <Navigate to="/account/projects" replace /> },
-			{ path: "projects", element: <Projects /> },
+			{
+				path: "projects",
+				element: <ProjectContainer />,
+				children: [
+					{ index: true, element: <Projects /> },
+					{
+						path: ":projectID",
+						element: <ProjectDetails />,
+						children: [
+							{ index: true, element: <></> },
+							{
+								path: "tasks",
+								children: [
+									{ index: true, element: <Navigate to="../" /> },
+									{
+										path: "new",
+										// Render `NewTask` component
+										element: <NewTask />,
+									},
+									{
+										path: ":taskID",
+										children: [
+											{ index: true, element: <>Show Task Details</> },
+										],
+									},
+								],
+							},
+						],
+					},
+				],
+			},
 			{ path: "members", element: <MembersPage /> },
 		],
 	},
