5f0946dae4f09af45fcd957d32794ed2ed6a5c7e
Praneeth <praneeth@PRABHAASINI>
2025-10-14 15:56:10 +0000

    L3 milestone

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -3,15 +3,21 @@ import "./TaskCard.css";
 
 interface TaskProp {
 	title: String;
+	description: String;
+	dueDate: Date;
 }
 
 class Task extends React.Component<TaskProp> {
 	render() {
 		return (
 			<div className="TaskItem shadow-md border border-slate-100">
-				<h2 className="text-base font-bold my-1">{this.props.title}</h2>
-				<p className="text-sm text-slate-500">Due Date:</p>
-				<p className="text-sm text-slate-500">Description:</p>
+				<h3 className="text-base font-bold my-1">{this.props.title}</h3>
+				<p className="text-sm text-slate-500">
+					Due Date: {this.props.dueDate.toLocaleDateString()}
+				</p>
+				<p className="text-sm text-slate-500">
+					Description: {this.props.description}
+				</p>
 			</div>
 		);
 	}
--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -5,12 +5,16 @@ interface TaskFormProps {
 }
 interface TaskFormState {
 	title: string;
+	description: string;
+	dueDate: Date;
 }
 class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
 	constructor(props: TaskFormProps) {
 		super(props);
 		this.state = {
 			title: "",
+			description: "",
+			dueDate: new Date(),
 		};
 	}
 
@@ -18,23 +22,50 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
 		event.preventDefault();
 		const newTask = {
 			title: this.state.title,
+			description: this.state.description,
+			dueDate: this.state.dueDate,
 		};
 		this.props.addTask(newTask);
-		this.setState({ title: "" });
+		this.setState({ title: "", description: "", dueDate: new Date() });
 	};
 
 	titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
 		console.log(`${event.target.value}`);
 		this.setState({ title: event.target.value });
 	};
+
+	descChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+		console.log(`${event.target.value}`);
+		this.setState({ description: event.target.value });
+	};
+
+	dateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+		console.log(`${new Date(event.target.value).toLocaleDateString()}`);
+		this.setState({ dueDate: new Date(event.target.value) });
+	};
 	render() {
 		return (
 			<form onSubmit={this.addTask}>
 				<input
 					type="text"
+					id="todoTitle"
 					value={this.state.title}
 					onChange={this.titleChanged}
 					required
+					placeholder="Title"
+				/>
+				<input
+					type="text"
+					id="todoDescription"
+					value={this.state.description}
+					onChange={this.descChanged}
+					placeholder="Description"
+				/>
+				<input
+					type="date"
+					id="todoDueDate"
+					value={this.state.dueDate.toISOString().split("T")[0]}
+					onChange={this.dateChanged}
 				/>
 				<button type="submit">Add item</button>
 			</form>
